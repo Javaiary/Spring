8a79042617f0db9a4d7aeb658e409f19687f42d1
Javaiary <narrchoii@gmail.com>
2022-11-01 23:42:33 +0900

    index page, userDao/JDBC

---
+++ SpringDAOWeb/src/main/java/kr/co/work/AopMain.java
@@ -0,0 +1,63 @@
+package kr.co.work;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.transaction.annotation.Transactional;
+
+public class AopMain {
+	public static void main(String[] args) throws Exception {
+		MyAdvice myAdvice = new MyAdvice();
+		
+		Class myClass = Class.forName("kr.co.work.MyClass");
+		Object obj = myClass.newInstance();
+		
+		for(Method m : myClass.getDeclaredMethods()) {
+			myAdvice.invoke(m, obj, null);
+		}
+	}
+
+}
+
+class MyClass {
+	@Transactional
+	void ezen1() {
+		System.out.println("ezen1() is called");
+	}
+
+	void ezen2() {
+		System.out.println("ezen2() is called");
+	}
+
+	void itezen1() {
+		System.out.println("itezen1() is called");
+	}
+}
+// 보조기능
+
+class MyAdvice {
+	Pattern p = Pattern.compile("e.*");
+	
+	boolean matches(Method m) {
+		Matcher matcher = p.matcher(m.getName());
+		return matcher.matches();	// e로 시작하는 메서드만 호출
+	}
+	
+	void invoke(Method m, Object obj, Object... args) throws Exception {
+		
+//		if(matches(m))	System.out.println("[이전 before] {");	//e로시작하는 메서드에만 호출됨
+		//핵심 기능에 Transactional 어노테이션이 설정되어 있는 경우에만 before/after를 출력ㄱ
+		if(m.getAnnotation(Transactional.class) != null)
+			System.out.println("[이전 before] {");
+		
+		m.invoke(obj,args);						// ezen1(), ezen2(), itezen1() 호출 가능
+		
+//		if(matches(m))	System.out.println("} [이후 after]");
+		if(m.getAnnotation(Transactional.class) != null)
+			System.out.println("} [이후 after]");
+
+
+	}
+}
---
+++ SpringDAOWeb/src/main/java/kr/co/work/RegisterController.java
@@ -0,0 +1,69 @@
+package kr.co.work;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+	@Autowired
+	UserDao userDao;
+	
+	final int FAIL = 0;
+	
+	//@RequestMapping대신에 @GetMapping,@PostMapping 사용 가능(데이터를 적게 먹음)
+	
+	@GetMapping("/add") //Get방식만 쓸 경우
+	public String register() {
+		return "registerForm";			//\webapp\WEB-INF\views\registerForm.jsp
+	}
+	@InitBinder
+	public void registerValidate(WebDataBinder binder) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class,new CustomDateEditor(df, false));
+		// UserValdiator를 WebDataBinder의 로컬 validator로 등록(수동)
+		binder.setValidator(new UserValidator());
+	}
+	
+	@PostMapping("/add")
+		public String save(@Valid User user,BindingResult result, Model m) throws UnsupportedEncodingException {
+		//1. 유효성 검사 => 관심사로 분리
+		System.out.println("result = " + result);
+		System.out.println("user = " + user);
+		
+		
+		//2. DB에 새 회원 정보를 저장
+		// User 객체를 검증한 결과 에러가 있으면 (필수입력 미기재 등), registerForm을 이용해 에러를 보여주어야 함
+		if(!result.hasErrors()) {
+			int rowCnt = userDao.insertUser(user);
+			
+			if(rowCnt != FAIL) return "registerInfo";
+		}
+		
+		return "registerForm";
+	}
+	private boolean isValid(User user) {
+	// 현재 유효하지 못하게 false로 설정함
+	return false;
+}
+
+	public String save(User user) {
+
+		return "registerInfo";			//\webapp\WEB-INF\views\registerInfo.jsp
+	}
+}

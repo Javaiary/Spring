b3f63541c75b9ed6ed1e11023957e96d033598f1
Javaiary <narrchoii@gmail.com>
2022-12-14 15:59:29 +0900

    spring security default settings

---
+++ SpringSecurityWeb/src/main/java/kr/co/springsecurity/SampleController.java
@@ -0,0 +1,15 @@
+package kr.co.springsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping
+@Controller
+public class SampleController {
+
+	
+	
+}

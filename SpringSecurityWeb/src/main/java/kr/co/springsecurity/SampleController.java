e782fe422e1bc2a117082261eb30b63f534bb162
Javaiary <narrchoii@gmail.com>
2022-12-15 15:23:28 +0900

    Simple login process

--- SpringSecurityWeb/src/main/java/kr/co/springsecurity/SampleController.java
+++ SpringSecurityWeb/src/main/java/kr/co/springsecurity/SampleController.java
@@ -1,15 +1,24 @@
 package kr.co.springsecurity;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
-@Log4j
-@RequestMapping
+@RequestMapping("/sample/*") 
 @Controller
 public class SampleController {
 
+	@GetMapping("/all")
+	public void doAll() {
+	}
 	
+	@GetMapping("/member")
+	public void doMember() {
+	}
 	
+	@GetMapping("/admin")
+	public void doAdmin() {
+	}
 }
